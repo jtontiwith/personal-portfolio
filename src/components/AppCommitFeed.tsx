b9984c4f17aba8f5101c6349faa9a8bc44b091d3
jtontiwith <jtonti@gmail.com>
2022-04-15 14:33:10 -0500

    transform date

--- src/components/AppCommitFeed.tsx
+++ src/components/AppCommitFeed.tsx
@@ -2,13 +2,18 @@ type CommitFeedProps = {
   commits: Array<any>
 }
 
+const transformDate = (isoDate) => {
+  const dArr = isoDate.split('T')[0].split('-')
+  return dArr[1] + '-' + dArr[2] + '-' + dArr[0]
+}
+
 const AppCommitFeed = ({ commits }: CommitFeedProps) => {
   const commitEls = commits.map((c, i) => {
     return (
       <div className="pt-2 mb-6 border-t border-gray-200" key={i}>
         <div>{c.type}</div>
         <div>{c.repo}</div>
-        <div>{c.created}</div>
+        <div>{transformDate(c.created)}</div>
         <div>{c.commits.map((c: { message: any }) => c.message)}</div>
       </div>
     )
